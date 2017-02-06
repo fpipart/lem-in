0f81b24a51f24a6cb38b28e9aac11f8fc98b34f4
Franck PIPART <fpipart@e1r8p1.42.fr>
2017-02-06 15:38:08 +0100

    test1 broken

--- lem_in_test.py
+++ lem_in_test.py
@@ -44,7 +44,7 @@ def test_12():
 
 if (len(sys.argv) == 1):
 	#test_0()
-	#test_1()
+	test_1()
 	#test_2()
 	#test_3()
 	#test_4()
@@ -53,5 +53,5 @@ if (len(sys.argv) == 1):
     #test_7()
     #test_8()
     #test_10()
-    test_11()
+    #test_11()
     #test_12()
--- srcs/display.c
+++ srcs/display.c
@@ -6,7 +6,7 @@
 /*   By: fpipart <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/31 10:44:07 by fpipart           #+#    #+#             */
-/*   Updated: 2017/02/06 12:33:13 by fpipart          ###   ########.fr       */
+/*   Updated: 2017/02/06 14:58:45 by fpipart          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,15 +35,19 @@ static int	move_ant_one_path(t_lem *new_map, int path, t_store *store)
 	t_lem *tmp_path;
 
 	tmp_path = new_map;
-	while (tmp_path->busy != path)
+	print_room(new_map);
+	ft_putendl("159------------");
+	ft_printf("path = %d\n", path);
+	while (tmp_path && tmp_path->busy != path)
 		tmp_path = tmp_path->next;
-	if (!ft_isdigit(*(tmp_path->ant)) || ft_isdigit(*(tmp_path->next->ant)))
-		while (tmp_path->next && (tmp_path->busy != path ||
-					!ft_isdigit(*(tmp_path->next->ant))))
-			tmp_path = tmp_path->next;
+//	if (!ft_isdigit(*(tmp_path->ant)) || ft_isdigit(*(tmp_path->next->ant)))
+//		while (tmp_path->next && (tmp_path->busy != path ||
+//					!ft_isdigit(*(tmp_path->next->ant))))
+//			tmp_path = tmp_path->next;
 	tmp = tmp_path;
 	if (!tmp->next)
 		return (0);
+	ft_putendl("159");
 	while (tmp && tmp->busy == path)
 	{
 		tmp = tmp_path;
@@ -64,6 +68,7 @@ static int	move_ant_inside(t_lem *new_map, int path_nbr_max, t_store *store)
 
 	path = 1;
 	tmp = new_map;
+	ft_putendl("159------------");
 	while (path <= path_nbr_max)
 	{
 		move_ant_one_path(new_map, path, store);
@@ -75,26 +80,31 @@ static int	move_ant_inside(t_lem *new_map, int path_nbr_max, t_store *store)
 int			fill_result(t_lem *new_map, t_store *store, int *size_paths)
 {
 	int	path_nbr_max;
-	int end_nbr;
+//	int end_nbr;
 
 	path_nbr_max = 0;
 	store->ants_tot = store->ants;
 	store->ants_strt = store->ants;
 	store->ants_end = 0;
 	store->ants = 1;
+	print_room(new_map);
 	while (*(size_paths + (path_nbr_max)) != -1)
 		path_nbr_max++;
+	path_nbr_max = 1;
 	while (store->ants_strt != 0)
 	{
+		ft_putendl("Start");
 		move_ant_inside(new_map, path_nbr_max, store);
+		ft_putendl("MID");
 		move_ants(new_map, store, size_paths, path_nbr_max);
+		ft_putendl("End");
 		ft_putchar('\n');
 	}
-	while (store->ants_end != store->ants_tot)
-	{
-		end_nbr = move_ant_inside_end(&new_map, path_nbr_max, store);
-		ft_putchar('\n');
+//	while (store->ants_end != store->ants_tot)
+//	{
+//		end_nbr = move_ant_inside_end(&new_map, path_nbr_max, store);
+//		ft_putchar('\n');
 //		print_resolution(new_map, end_nbr);
-	}
+//	}
 	return (0);
 }
